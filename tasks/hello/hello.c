ec23df1a29ffd16dbee732f93ef6a3d4d9a869ee
Sanjay K J <sjayasan@andrew.cmu.edu>
2013-11-24 19:41:01 -0500

    Modified swi_handler for restoring r8using global_data tested with hello, splat & typo removed r8,lr saving by switchToUser function

--- tasks/hello/hello.c
+++ tasks/hello/hello.c
@@ -8,11 +8,23 @@
  * @date   2008-10-29
  */
 #include <unistd.h>
+#include <stdio.h>
 
 const char hello[] = "Hello World\r\n";
-
-int main(int argc, char** argv)
+char str[40];
+//char str1[40];
+//int main(int argc, char** argv)
+int main(void)
 {
-	write(STDOUT_FILENO, hello, sizeof(hello) - 1);
+    unsigned int a;
+    a=read(STDIN_FILENO, str, 20);
+
+        
+//    printf("\nret2=%lu\n",a);
+   // printf("\nret3=%d\n",sizeof(str));
+    write(STDOUT_FILENO, str, a);
+    write(STDOUT_FILENO, "\n", 1);
+//    printf("\njj");
+	//write(STDOUT_FILENO, hello, sizeof(hello) - 1);
 	return 0;
 }
